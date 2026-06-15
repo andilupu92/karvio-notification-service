d92a310dc1f4f4533dcb9f87e5962229e7872e9f
andilupu92 <andilupu92@gmail.com>
2026-06-15 15:20:48 +0300

    new abordation for firebase file config

--- src/main/java/karvio/security/FirebaseConfig.java
+++ src/main/java/karvio/security/FirebaseConfig.java
@@ -4,18 +4,23 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_CREDENTIALS_JSON}")
+    private String firebaseCredentialsJson;
+
     @PostConstruct
     public void initialize() throws IOException {
-        InputStream serviceAccount = new ClassPathResource("karvio-app-d8872-firebase-adminsdk-fbsvc-272cdc1940.json").getInputStream();
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseCredentialsJson.getBytes(StandardCharsets.UTF_8));
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
